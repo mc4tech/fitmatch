da46dbfcb39c578648b55d3da55d9b7ffd682904
Menelik Tefera <menelikworku@gmail.com>
2017-10-19 04:28:20 -0400

    route edited

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const usersController = require("../../controllers/usersController");
+const usersController = require("../../controllers/usersController.js");
 
 // Matches with "/api/users"
 router.route("/")
