38994fe7d37013c3c2dd3c5e04941496523070fa
Menelik Tefera <menelikworku@gmail.com>
2017-10-19 14:35:22 -0400

    deployment callback URL edited

--- client/src/pages/Auth/auth0-variables.js
+++ client/src/pages/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'menelik.auth0.com',
   clientId: 'w75HS7gZ5El56WaVnuUuk0TJdfpdcfow',
-  callbackUrl: 'https://gt-fitmatch.herokuapp.com/callback',
+  callbackUrl: 'http://localhost:3000/callback'
 }
