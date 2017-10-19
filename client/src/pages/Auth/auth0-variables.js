593a4bc3a032605954de772b5dcd659f0a6983f9
Menelik Tefera <menelikworku@gmail.com>
2017-10-19 15:05:44 -0400

    passprot added

--- client/src/pages/Auth/auth0-variables.js
+++ client/src/pages/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'menelik.auth0.com',
   clientId: 'w75HS7gZ5El56WaVnuUuk0TJdfpdcfow',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://gt-fitmatch.herokuapp.com/callback'
 }
