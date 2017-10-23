5882f73ea5d6b8a6e9caa7c61c2f1eb79645b429
Menelik Tefera <menelikworku@gmail.com>
2017-10-23 00:04:22 -0400

    match attributes styled

--- client/src/pages/Auth/auth0-variables.js
+++ client/src/pages/Auth/auth0-variables.js
@@ -1,5 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'menelik.auth0.com',
   clientId: 'w75HS7gZ5El56WaVnuUuk0TJdfpdcfow',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://gt-fitmatch.herokuapp.com/callback'
 }
+
+// http://localhost:3000/callback
